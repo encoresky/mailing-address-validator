1c39b5942a410a74d7cf64e02955c83122f08e22
Fakirchand Patidar <fc.patidar@encoresky.com>
2023-01-19 23:59:22 +0530

    Change README and DB config

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # mailing-address-validator
 Validate/Standardize addresses using USPS
+
+# DB Config
+Open db.php file and modify the DB information
+
+DB_HOST: 
+DB_USERNAME: 
+DB_PASSWORD: 
+DB_NAME: 
--- db.php
+++ db.php
@@ -3,10 +3,15 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$servername = "localhost";
-$username = "root";
-$password = "root";
-$dbname = "mailing_db";
+define("DB_HOST", "localhost");
+define("DB_USERNAME", "root");
+define("DB_PASSWORD", "root");
+define("DB_NAME", "mailing_db");
+
+$servername = DB_HOST;
+$username = DB_USERNAME;
+$password = DB_PASSWORD;
+$dbname = DB_NAME;
 
 $mysqli = new mysqli($servername, $username, $password, $dbname);
 
